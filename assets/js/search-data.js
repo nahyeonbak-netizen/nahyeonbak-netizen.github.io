e3ffcda947f12327ad31c281ab5f6624354e9a91
nahyeonbak-netizen <nahyeonbak@gmail.com>
2026-03-27 19:50:45 +0000

    Deploying to gh-pages from @ nahyeonbak-netizen/nahyeonbak-netizen.github.io@7c492ddb744ddc3a7d92975170ca155cb7d3edb6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-resume",
           title: "resume",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "Click the PDF download button to download my full resume.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
